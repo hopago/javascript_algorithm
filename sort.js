7127420859917476246f7def967581433f38ac5f
hopago <132269386+hopago@users.noreply.github.com>
2024-12-01 17:24:01 +0900

    힙, 퀵, 기수 정렬

--- sort.js
+++ sort.js
@@ -6,7 +6,7 @@
 // 버블 정렬 O(n^2)
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 1; j < array.length; j++) {
+        for (let j = i + 1; j < array.length; j++) {
             if (array[j - 1] > array[j]) {
                 const temp = array[j]
                 array[j] = array[j - 1];
@@ -22,7 +22,7 @@ const selectionSort = (array) => {
     let indexMin, temp;
     for (let i = 0; i < array.length - 1; i++) {
         indexMin = i;
-        for (let j = 1; j < array.length; j++) {
+        for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[indexMin]) {
                 indexMin = j;
             }
@@ -71,8 +71,8 @@ const mergeSort = (array) => {
     if (array.length <= 1) return array;
 
     const mid = Math.floor(array.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
     const sortedLeft = mergeSort(left);
     const sortedRight = mergeSort(right);
     return merge(sortedLeft, sortedRight)
@@ -102,3 +102,141 @@ const merge = (leftArr, rightArr) => {
     return merged;
 };
 
+// 퀵 정렬 O(n log n) - 최악: O(n^2)
+
+const quickSort = (array) => {
+    if (array.length <= 1) return array;
+
+    const pivot = array[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i])
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+// 힙 정렬
+
+/** 힙(HEAP)이란?
+ * 여러 개의 값 중에서 가장 크거나 작은 값을 빠르게 찾기 위해 만든 이진 트리
+ * 짧게 HEAP이라고 부르기도 한다
+ */
+
+/** 최대 힙
+ * 각 노드의 값이 해당 노드의 자식 노드들의 값보다 크거나 같다
+ * 루트 노드에는 전체 힙에서 가장 큰 값이 위치한다
+ * 우선순위 큐에서는 가장 큰 값이 우선적으로 처리돼야 할 때 사용된다
+ */
+
+/** 최소 힙
+ * ! 작거나 같다
+ * ! 가장 작은 값이 위치한다
+ * ! 가장 작은 값이 우선적으로 처리돼야 할 때
+ */
+
+var arr = [6, 3, 8, 2, 10, 5];
+var sortedArr = heapSort(arr);
+console.log(sortedArr);
+
+function heapSort(arr) {
+    // 힙 구성
+    function buildHeap(arr) {
+        const len = arr.length;
+        for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+            heapify(arr, len, i)
+        }
+    }
+    // 힙 유지
+    function heapify(arr, len, idx) {
+        let largest = idx;
+        const left = 2 * idx + 1;
+        const right = 2 * idx + 2;
+        if (left < len && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < len && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest !== idx) {
+            [arr[idx], arr[largest]] = [arr[largest], arr[idx]]
+            heapify(arr, len, largest)
+        }
+    }
+    // 힙 정렬
+    function sort(arr) {
+        const len = arr.length;
+        buildHeap(arr);
+        for (let i = len - 1; i >= 0; i--) {
+            [arr[0], arr[i]] = [arr[i], arr[0]];
+            heapify(arr, i, 0)
+        }
+        return arr;
+    }
+    return sort(arr)
+}
+
+// 기수 정렬
+
+/** 기수 정렬 순서
+ * 가장 작은 자릿수부터 가장 큰 자릿수까지 반복하여 비교
+ * 각 자릿수를 기준으로 입력 배열을 정렬
+ * 각 자릿수별로 정렬된 배열을 합쳐 정렬 완료
+ */
+
+function radixSort(arr) {
+    const countingSort = (arr, exp) => {
+        const output = new Array(arr.length).fill(0);
+        const count = new Array(10).fill(0);
+        arr.forEach((num) => {
+            const digit = Math.floor(num / exp) % 10;
+            count[digit]++;
+        })
+        console.log("count: ", count);
+        for (let i = 1; i < count.length; i++) {
+            count[i] += count[i - 1]
+        }
+        console.log("updatedCount: ", count);
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const digit = Math.floor(arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+        console.log("finalizedCount: ", count);
+        output.forEach((num, i) => {
+            arr[i] = num
+        })
+    }
+    const max = Math.max(...arr);
+    const maxDigit = Math.floor(Math.log10(max) + 1);
+    for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+        countingSort(arr, exp)
+    }
+    return arr;
+}
+
+console.clear()
+console.log(radixSort([25, 36, 1, 3, 100, 1002, 39, 121]))
+
+/** .sort()
+ * 크롬 - TIM SORT
+ * 사파리 - MERGE SORT
+ */
+
+const items = [
+    { key: "봄", value: 10 },
+    { key: "여름", value: 30 },
+    { key: "가을", value: 70 },
+    { key: "겨울", value: 20 },
+]
+
+console.log(items.sort((a, b) => {
+    if (a.value > b.value) {
+        return 1;
+    }
+    if (a.value < b.value) {
+        return -1;
+    }
+    return 0;
+}))
