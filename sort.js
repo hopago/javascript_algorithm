dd549b58b2f6a3d8798781c7e9a45d28f45c7e5b
hopago <132269386+hopago@users.noreply.github.com>
2025-01-03 18:02:38 +0900

    이진트리 예제

--- sort.js
+++ sort.js
@@ -388,7 +388,7 @@ function merge(left, right) {
 
 // 퀵 정렬 O(n log n) - 최악: O(n^2)
 
-function quickSort (array) {
+function quickSort(array) {
     if (array.length <= 1) return array;
 
     const pivot = array[0];
@@ -513,6 +513,51 @@ function heapSort(arr) {
     }
     return sort(arr)
 }
+
+// 예제 1
+let treasureBoxes = Array.from({ length: 100 }, () => {
+    return Math.floor(Math.random() * 100)
+});
+
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest !== i) {
+        let swap = arr[i]
+        arr[i] = arr[largest]
+        arr[largest] = swap;
+
+        heapify(arr, n, largest)
+    }
+}
+
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i)
+    }
+
+    for (let i = n - 1; i > 0; i--) {
+        let temp = arr[0];
+        arr[0] = arr[i]
+        arr[i] = temp;
+        heapify(arr, i , 0)
+    }
+}
+
+heapSort(treasureBoxes);
+console.log(treasureBoxes.reverse())
 
 // 기수 정렬
 
